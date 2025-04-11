3c9ea025dc6f3edbb8e708d5d3652dcda44fc75c
Thiago Cruz <thiagocruz.eu@gmail.com>
2025-04-11 01:57:44 -0300

    chore: config eslint, update prettier rule and fix styling

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,6 +1,6 @@
 export default {
-  "*.{js,ts,mjs,mts}": (filenames) => [
-    `npx prettier --write ${filenames.join(" ")}`,
+  '*.{js,ts,mjs,mts}': (filenames) => [
+    `npx prettier --write ${filenames.join(' ')}`,
     // 'eslint . --fix --ignore-pattern "dist/*"',
     // config testing routine...,
   ],
