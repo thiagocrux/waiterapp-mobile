14c143b641acb0050c559bf84e54700032e574cd
Thiago Cruz <thiagocruz.eu@gmail.com>
2025-04-11 11:22:12 -0300

    feat: create initial app structure

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,11 +1,28 @@
+import { Categories } from '../components/Categories';
 import { Header } from '../components/Header';
+import { Menu } from '../components/Menu';
 
-import { Container } from './styles';
+import {
+  CategoriesContainer,
+  Container,
+  Footer,
+  FooterContainer,
+  MenuContainer,
+} from './styles';
 
 export function Main() {
   return (
     <Container>
       <Header />
+      <CategoriesContainer>
+        <Categories />
+      </CategoriesContainer>
+      <MenuContainer>
+        <Menu />
+      </MenuContainer>
+      <Footer>
+        <FooterContainer></FooterContainer>
+      </Footer>
     </Container>
   );
 }
