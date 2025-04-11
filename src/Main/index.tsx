34fac7f719d054147673c681189fbc86f280c228
Thiago Cruz <thiagocruz.eu@gmail.com>
2025-04-11 11:12:02 -0300

    feat: create header component

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,11 +1,11 @@
-import { Text } from '../components/Text';
+import { Header } from '../components/Header';
 
 import { Container } from './styles';
 
 export function Main() {
   return (
     <Container>
-      <Text>Hello, world!</Text>
+      <Header />
     </Container>
   );
 }
