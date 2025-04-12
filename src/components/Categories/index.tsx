6477f9d7e1b7e129979f544f30bb0a1b013ef34c
Thiago Cruz <thiagocruz.eu@gmail.com>
2025-04-12 10:27:47 -0300

    feat: create categories section

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,3 +1,40 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
+import { categories } from '../../mocks/categories';
+import { Text } from '../Text';
+
+import { Category, Icon } from './styles';
+
 export function Categories() {
-  return null;
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  function handleCategorySelection(categoryId: string) {
+    const category = selectedCategory === categoryId ? '' : categoryId;
+    setSelectedCategory(category);
+  }
+
+  return (
+    <FlatList
+      data={categories}
+      keyExtractor={(category) => category._id}
+      renderItem={({ item: category }) => {
+        const isSelected = selectedCategory === category._id;
+
+        return (
+          <Category onPress={() => handleCategorySelection(category._id)}>
+            <Icon>
+              <Text opacity={isSelected ? 1 : 0.5}>{category.icon}</Text>
+            </Icon>
+            <Text size={14} weight="600" opacity={isSelected ? 1 : 0.5}>
+              {category.name}
+            </Text>
+          </Category>
+        );
+      }}
+      horizontal
+      contentContainerStyle={{ paddingRight: 24 }}
+      showsHorizontalScrollIndicator={false}
+    />
+  );
 }
